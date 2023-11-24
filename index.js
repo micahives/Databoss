9a95c1a90cab66ee6453ab985ae66b4aa10abfb1
Micah Ives <micbenives@gmail.com>
2023-11-24 12:36:26 -0600

    Add inquirer prompt and set database structure

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
+
+function init() {
+    inquirer.prompt([
+        {
+        type: 'list',
+        name: 'action',
+        message: 'What would you like to do?',
+        choices: [
+            'view all departments',
+            'view all roles',
+            'view all employees',
+            'add a department',
+            'add a role',
+            'add an employee',
+            'update an employee role'
+          ]
+        }
+    ])
+}
+
+init();
