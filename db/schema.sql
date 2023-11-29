076ab775532e6235d00aeb1c75c8d7b3d729e7b0
Micah Ives <micbenives@gmail.com>
2023-11-28 21:05:18 -0600

    Update add role and employee functionality

--- db/schema.sql
+++ db/schema.sql
@@ -4,12 +4,12 @@ CREATE DATABASE employees_db;
 USE employees_db;
 
 CREATE TABLE department (
-    id INT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30)
 );
 
 CREATE TABLE role (
-    id INT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30),
     salary DECIMAL(10, 2),
     department_id INT,
@@ -17,7 +17,7 @@ CREATE TABLE role (
 );
 
 CREATE TABLE employee (
-    id INT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INT,
@@ -25,4 +25,3 @@ CREATE TABLE employee (
     FOREIGN KEY (role_id) REFERENCES role(id),
     FOREIGN KEY (manager_id) REFERENCES employee(id)
 );
-
--- index.js
+++ index.js
@@ -4,14 +4,14 @@ const mysql = require('mysql2/promise');
 const Table = require('cli-table');
 require('dotenv').config();
 
-
 async function connectToDatabase() {
     const db = await mysql.createConnection(
         {
             host: process.env.host,
             user: process.env.user,
             password: process.env.password,
-            database: process.env.database
+            database: process.env.database,
+            multipleStatements: true,
         });    
 
         console.log(`Connected to ${process.env.database}`);
@@ -51,92 +51,192 @@ async function init() {
 
         // Close the database connection when done
         await db.end();
-        
+
     } catch (error) {
         console.error('Error connecting to the database:', error);
     }
 }
 
-
 async function generateQuery(response, db) {
-    if (response.action === 'View all departments') {
-        const [results, fields] = await db.query('SELECT * FROM department');
+    try {
+        if (response.action === 'View all departments') {
+            const [results, fields] = await db.query('SELECT * FROM department');
+    
+            // Extract column names from the fields array
+            const columns = fields.map(field => field.name);
+    
+            // Create table in the CLI
+            const table = new Table({
+                head: columns
+            });
+    
+            results.forEach(row => {
+                // CLI-table throws error if data value is 'null', handles string output of 'null' if value is null in mysql
+                table.push(columns.map(column => row[column] === null ? 'null' : row[column]));
+            });
+    
+            // Display the table in the CLI
+            console.log(table.toString());
+    
+        } else if (response.action === 'View all roles') {
+            const [results, fields] = await db.query(
+                `SELECT
+                    role.title AS job_title, 
+                    role.id,
+                    department.name AS department, 
+                    role.salary
+                FROM 
+                    role
+                JOIN 
+                    department ON role.department_id = department.id`
+                );
+    
+            const columns = fields.map(field => field.name);
+    
+            const table = new Table({
+                head:columns
+            });
+    
+            results.forEach(row => {
+                table.push(columns.map(column => row[column] === null ? 'null' : row[column]));
+            });
+    
+            console.log(table.toString());
+    
+    
+        } else if (response.action === 'View all employees') {
+            const [results, fields] = await db.query(
+                `SELECT
+                    employee.id AS employee_id, 
+                    employee.first_name, 
+                    employee.last_name,
+                    role.title AS job_title,
+                    department.name AS department,
+                    role.salary,
+                    CONCAT(manager.first_name, ' ', manager.last_name) AS manager_name
+                FROM 
+                    employee
+                JOIN 
+                    role ON employee.role_id = role.id
+                JOIN 
+                    department ON role.department_id = department.id
+                LEFT JOIN 
+                    employee AS manager ON employee.manager_id = manager.id`
+            );
+    
+            const columns = fields.map(field => field.name);
+    
+            const table = new Table({
+                head:columns
+            });
+    
+            results.forEach(row => {
+                table.push(columns.map(column => row[column] === null ? 'null' : row[column]));
+            });
+    
+            console.log(table.toString());
+        } else if (response.action === 'Add a department') {
+            const userInput = await inquirer.prompt([
+                {
+                    type: 'input',
+                    name: 'enterDepartment',
+                    message: 'Enter the department name'
+                }
+            ]);
+    
+            const [results, fields] = await db.query(
+                'INSERT INTO department (name) VALUES (?)',
+                [userInput.enterDepartment]
+            );
+    
+            console.log('Department added successfully, select "View all departments" for the updated entry');
+        } else if (response.action === 'Add a role') {
+            const userInput = await inquirer.prompt([
+                {
+                    type: 'input',
+                    name: 'enterRole',
+                    message: 'Enter the role name'
+                },
+                {
+                    type: 'input',
+                    name: 'enterSalary',
+                    message: 'Enter the role salary'  
+                },
+                {
+                    type: 'input',
+                    name: 'enterRoleDepartment',
+                    message: 'Enter the department for the role'
+                }
+            ]);
 
-        // Extract column names from the fields array
-        const columns = fields.map(field => field.name);
-
-        // Create table in the CLI
-        const table = new Table({
-            head: columns
-        });
-
-        results.forEach(row => {
-            // CLI-table throws error if data value is 'null', handles string output of 'null' if value is null in mysql
-            table.push(columns.map(column => row[column] === null ? 'null' : row[column]));
-        });
-
-        // Display the table in the CLI
-        console.log(table.toString());
-
-    } else if (response.action === 'View all roles') {
-        const [results, fields] = await db.query(
-            `SELECT
-                role.title AS job_title, 
-                role.id,
-                department.name AS department, 
-                role.salary
-            FROM 
-                role
-            JOIN 
-                department ON role.department_id = department.id`
+            const [departmentResults, departmentFields] = await db.query(
+                'INSERT INTO department (name) VALUES (?)',
+                [userInput.enterRoleDepartment]
             );
 
-        const columns = fields.map(field => field.name);
+            const departmentId = departmentResults.insertId;
 
-        const table = new Table({
-            head:columns
-        });
+            const [roleResults, roleFields] = await db.query(
+                'INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)',
+                [userInput.enterRole, userInput.enterSalary, departmentId]
+            );
 
-        results.forEach(row => {
-            table.push(columns.map(column => row[column] === null ? 'null' : row[column]));
-        });
+            console.log('Role added successfully, select "View all roles" for the updated entry');
+        } else if (response.action === 'Add an employee') {
+            const userInput = await inquirer.prompt([
+                {
+                    type: 'input',
+                    name: 'enterFirstName',
+                    message: "Enter the employee's first name"
+                },
+                {
+                    type: 'input',
+                    name: 'enterLastName',
+                    message: "Enter the employee's last name"
+                },
+                {
+                    type: 'input',
+                    name: 'enterEmployeeRole',
+                    message: "Enter the employee's role"
+                },
+                {
+                    type: 'input',
+                    name: 'enterManagerFirstName',
+                    message: "Enter the manager's first name"
+                },
+                {
+                    type: 'input',
+                    name: 'enterManagerLastName',
+                    message: "Enter the manager's last name"
+                }
+            ]);
 
-        console.log(table.toString());
+            await db.query('START TRANSACTION');
 
+            const [roleResults, roleFields] = await db.query(
+                'INSERT INTO role (title) VALUES (?)',
+                [userInput.enterEmployeeRole]
+            );
 
-    } else if (response.action === 'View all employees') {
-        const [results, fields] = await db.query(
-            `SELECT
-                employee.id AS employee_id, 
-                employee.first_name, 
-                employee.last_name,
-                role.title AS job_title,
-                department.name AS department,
-                role.salary,
-                CONCAT(manager.first_name, ' ', manager.last_name) AS manager_name
-            FROM 
-                employee
-            JOIN 
-                role ON employee.role_id = role.id
-            JOIN 
-                department ON role.department_id = department.id
-            LEFT JOIN 
-                employee AS manager ON employee.manager_id = manager.id`
-        );
+            const roleId = roleResults.insertId;
 
-        const columns = fields.map(field => field.name);
+            const [managerResults, managerFields] = await db.query(
+                'INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, NULL)',
+                [userInput.enterManagerFirstName, userInput.enterManagerLastName, roleId]
+            );
 
-        const table = new Table({
-            head:columns
-        });
+            const managerId = managerResults.insertId;
 
-        results.forEach(row => {
-            table.push(columns.map(column => row[column] === null ? 'null' : row[column]));
-        });
+            const [employeeResults, employeeFields] = await db.query(
+                'INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)',
+                [userInput.enterFirstName, userInput.enterLastName, roleId, managerId]
+            );
 
-        console.log(table.toString());
+            console.log('Employee added successfully, select "View all employees" for the updated entry');
+        }
+    } catch (error) {
+        console.error('Error executing query:', error);
     }
 }
-
 
 init();
